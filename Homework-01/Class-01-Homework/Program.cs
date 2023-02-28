ef757cf83f5a9942180904be23a47f2bab3a9bf3
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-02-28 16:02:57 +0100

    Add files via upload

---
+++ Homework-01/Class-01-Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercise 1\n");
+
+Console.WriteLine("    * ");
+Console.WriteLine("   ***");
+Console.WriteLine("  *****");
+Console.WriteLine(" *******");
+Console.WriteLine("*********\n");
+
+
+Console.WriteLine("Exercise 2\n");
+
+
+Console.WriteLine("   *");
+Console.WriteLine("   *");
+Console.WriteLine(" * * *");
+Console.WriteLine("*  *  *");
+Console.WriteLine("   *");
+Console.WriteLine("   *");
+Console.WriteLine(" *   *");
+Console.WriteLine("*     *");
+
+Console.ReadLine();
