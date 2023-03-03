0d215745dcd597f053b1a1a97923bb689d144892
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-03-03 01:00:05 +0100

    Add files via upload

---
+++ Homework-02/SEDC.ClassExercises/SEDC.ClassExercises/Program.cs
@@ -0,0 +1,80 @@
+﻿Console.WriteLine("Exercise 5\n");
+
+int numberOfBranches = 12;
+int numberOfApples = 8;
+double basketCapacity = 5;
+
+Console.WriteLine("Please enter number of trees:");
+bool userInput = int.TryParse(Console.ReadLine(), out int numberOfTrees);
+
+if (userInput)
+{
+    int numberOfTotalBranches = numberOfBranches * numberOfTrees;
+    int numberOfTotalApples = numberOfTotalBranches * numberOfApples;
+    double numberOfBaskets = numberOfTotalApples / basketCapacity;
+
+    if (numberOfBaskets % 2 != 0)
+    {
+        numberOfBaskets = Math.Ceiling(numberOfBaskets);
+    }
+    Console.WriteLine("You will need " + numberOfBaskets + " baskets to collect all the apples.\n");
+}
+
+Console.WriteLine("Exercise 6\n");
+
+Console.WriteLine("Please enter the first number:");
+int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number:");
+int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+if (userInput1 > userInput2)
+{
+    Console.WriteLine("The bigger number is: " + userInput1);
+} else if(userInput1 < userInput2)
+{
+    Console.WriteLine("The bigger number is " + userInput2);
+} else
+{
+    Console.WriteLine("The numbers are equal");
+}
+
+if (userInput1 % 2 == 0)
+{
+    Console.WriteLine("The number " + userInput1 + " is even");
+} else
+{
+    Console.WriteLine("The number " + userInput1 + " is odd");
+}
+
+if(userInput2 % 2 == 0)
+{
+    Console.WriteLine("The number " + userInput2 + " is even");
+} else
+{
+    Console.WriteLine("The number " + userInput2 + " is odd\n");
+}
+
+Console.WriteLine("Exercise 7\n");
+
+Console.WriteLine("Please enter a number from 1 to 3:");
+int userInputNumber = Convert.ToInt16(Console.ReadLine());
+
+switch (userInputNumber)
+{
+    case 1:
+        Console.WriteLine("You got a new car!");
+        break;
+    case 2:
+        Console.WriteLine("You got a new plane!");
+        break;
+    case 3:
+        Console.WriteLine("You got a new bike!");
+        break;
+    default:
+        Console.WriteLine("Error: Please enter a number from 1 to 3!");
+        break;
+}
+
+
+Console.ReadLine();
---
+++ Homework-02/SEDC.Homework_2/SEDC.Homework_2/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.WriteLine("Task1\n");
+
+Console.WriteLine("Please enter first number:");
+int userNum1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter second number:");
+int userNum2 = int.Parse(Console.ReadLine());
+
+int sum = userNum1 + userNum2;
+int diff = userNum1 - userNum2;
+int multiply = userNum1 * userNum2;
+int divide = userNum1 / userNum2;
+
+Console.WriteLine("Please enter the operation:");
+string operation = Console.ReadLine();
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine("The result is: " + sum);
+        break;
+    case "-":
+        Console.WriteLine("The result is: " + diff);
+        break;
+    case "*":
+        Console.WriteLine("The result is " + multiply);
+        break;
+    case "/":
+        Console.WriteLine("The result is " + divide);
+        break;
+    default:
+        Console.WriteLine("You didn't enter a valid operation");
+        break;
+}
+
+Console.WriteLine("Task 2\n");
+
+Console.WriteLine("Enter the first number");
+double userInput1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+double userInput2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the third number");
+double userInput3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the fourth number");
+double userInput4 = Convert.ToInt32(Console.ReadLine());
+
+double printAverage = (userInput1 + userInput2 + userInput3 + userInput4) / 4;
+Console.WriteLine("The average of " + userInput1 + ", " + userInput2 + ", " + userInput3 + ", " + userInput4 + " is " + printAverage);
+
+Console.WriteLine("Task 3\n");
+
+Console.WriteLine("Input the first number:");
+int userNumber1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number:");
+int userNumber2 = int.Parse(Console.ReadLine());
+
+Console.Write("Before swapping: first number = " + userNumber1 + ", second number = " + userNumber2 + "\n");
+
+userNumber1 = userNumber1 + userNumber2;
+userNumber2 = userNumber1 - userNumber2;
+userNumber1 = userNumber1 - userNumber2;
+
+Console.WriteLine("After swapping: first number = " + userNumber1 + ", second number = " + userNumber2);
+
+
+Console.ReadLine();
