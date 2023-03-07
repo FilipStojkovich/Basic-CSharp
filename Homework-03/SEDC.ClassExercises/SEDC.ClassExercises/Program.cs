66fce5df0542095c333e4371eb81c4c71cca7733
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-03-07 19:40:12 +0100

    Add files via upload

---
+++ Homework-03/SEDC.ClassExercises/SEDC.ClassExercises/Program.cs
@@ -0,0 +1,63 @@
+﻿//Exercise 4
+
+string[] wordsArray = new string[5] { "This", "is", "the", "fourth", "exercise" };
+double[] decimalArray = new double[5] { 1.5, 2.5, 3.5, 4.5, 5.5 };
+char[] characterArray = new char[5] { 'F', 'i', 'l', 'i', 'p' };
+bool[] boolArray = new bool[5] { true, false, false, true, true };
+int[][] twoDArray = new int[2][] { new int[] { 1, 2 }, new int[] { 4, 5 } };
+
+//Exercise 5
+int[] integerArray = new int[5];
+
+Console.WriteLine("Please enter your first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+integerArray[0] = firstNumber;
+
+Console.WriteLine("Please enter your second number:");
+int secondNumber = int.Parse(Console.ReadLine());
+integerArray[1] = secondNumber;
+
+Console.WriteLine("Please enter your third number:");
+int thirdNumber = int.Parse(Console.ReadLine());
+integerArray[2] = thirdNumber;
+
+Console.WriteLine("Please enter your fourth number:");
+int fourthNumber = int.Parse(Console.ReadLine());
+integerArray[3] = fourthNumber;
+
+Console.WriteLine("Please enter your fifth number:");
+int fifthNumber = int.Parse(Console.ReadLine());
+integerArray[4] = fifthNumber;
+
+int sum = 0;
+foreach (int number in integerArray)
+{
+    sum += number;
+}
+
+Console.WriteLine("The sum of the numbers you entered is " + sum);
+
+//Exercise 6
+string[] namesArray = new string[10];
+int index = 0;
+string input = "";
+
+do
+{
+    Console.Write("Enter a name: ");
+    input = Console.ReadLine();
+    namesArray[index] = input;
+    index++;
+
+    Console.Write("Do you want to enter another name? (Y/N): ");
+    input = Console.ReadLine();
+}
+while (input.ToUpper() == "Y");
+
+Console.WriteLine("All entered names:");
+for (int i = 0; i < index; i++)
+{
+    Console.WriteLine(namesArray[i]);
+}
+
+Console.ReadLine();
---
+++ Homework-03/SEDC.Homework_03/SEDC.Homework_03/Program.cs
@@ -0,0 +1,46 @@
+﻿// Task 1
+ int[] intArray = new int[6];
+ int sum = 0;
+
+for(int i = 0; i < intArray.Length; i++)
+{
+    Console.WriteLine($"Enter integer no. {i + 1}");
+    int userInput = int.Parse(Console.ReadLine());
+
+    if(userInput % 2 == 0)
+    {
+        sum += userInput;
+    }
+}
+
+Console.WriteLine("The sum of the even numbers is " + sum);
+
+// Task 2
+
+string[] studentsG1 = new string[5] { "Filip", "Martin", "Mila", "Damjan", "Tosho" };
+string[] studentsG2 = new string[5] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+
+Console.WriteLine("Enter student group: (1/2)");
+int userInputGroup = int.Parse(Console.ReadLine());
+
+if (userInputGroup == 1)
+{
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (userInputGroup == 2)
+{
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number!");
+}
+
+
+Console.ReadLine();
